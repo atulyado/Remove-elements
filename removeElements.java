723e5aa9a48d7856835c0ef66b678794c0d631b9
atulyado <133553524+atulyado@users.noreply.github.com>
2025-06-20 19:25:30 +0700

    Update removeElements.java

--- removeElements.java
+++ removeElements.java
@@ -1,9 +1,9 @@
 class Solution {
-    public int removeElement(int[] nums, int val) {
+    public int removeElement(int[] nums, int val) {//nums = 1234  val = 2
         int k =0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] != val){
-            nums[k] = nums[i];
+        for(int i=0;i<nums.length;i++){//go through nums
+            if(nums[i] != val){//if nums is not equal 2 
+            nums[k] = nums[i];//it storage every nums not equal to 2 and add 1 to k
         k++;
             }
         }
